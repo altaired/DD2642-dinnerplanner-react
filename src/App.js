661709e35e7a3f4c5eb0431fa01b42e00adaf9d9
Simon Persson <simon@akep.se>
2019-02-19 20:27:26 +0100

    is is working

--- src/App.js
+++ src/App.js
@@ -12,7 +12,25 @@ import './App.css';
 import Welcome from './welcome/Welcome';
 import Search from './search/Search';
 import Dish from './dish/Dish';
-import store from './store';
+import {
+  createStore,
+  applyMiddleware,
+  compose
+} from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  ));
 
 
 class App extends Component {
@@ -23,21 +41,21 @@ class App extends Component {
     };
   }
 
+
   render() {
     return (
-      <Provider store={store}>
-        <div className="App">
+      <Provider store={store} >
+        <div className="App" >
           <header className="App-header" >
             <h1 className="App-title" > {
               this.state.title
-            }</h1>{
+            } </h1>{
               /* We rended diffrent component based on the path */
             }
             <Route exact path="/" component={Welcome} />
             <Route path="/search" component={Search} />
             <Route path="/dish/:id" component={Dish} />
-          </header>
-        </div>
+          </header> </div>
       </Provider>
     );
   }
